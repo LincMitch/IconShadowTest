173b85382e7f9fa6cafc250bf94882e1f5cf96df
LincAtPenCS <lincoln.mitchell@pencs.com.au>
2024-09-03 19:30:22 +0100

    Works
    The icon now appears. Next step is to get it working with icon.tokens

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -1,18 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { MaterialSymbol } from 'react-material-symbols';
-import 'react-material-symbols/rounded'; // Place in your root app file. There are also `sharp` and `outlined` variants.
 import root from 'react-shadow';
-
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>    
-      <p style={{color: "red"}}>This Icon is OUTSIDE the Shadow Dom:</p>
-      <MaterialSymbol icon="folder" size={24} fill grade={-25} color='red' />
-      <root.div className="quote">
-        <p style={{color: "lime"}}>This Icon is INSIDE the Shadow Dom, but only its name appears:</p>
-        <MaterialSymbol icon="folder" size={24} fill grade={-25} color='lime' />
+      This Icon is INSIDE the Shadow Dom:
+      <root.div>
+        <MaterialSymbol className="material-symbols-outlined" icon="folder" size={124} grade={-25} />
+        <link type="text/css" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet"/>
       </root.div>
     </div>
   )
