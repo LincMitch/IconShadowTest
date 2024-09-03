c2116b6d689559c584e476906ae9b426eeccb1fb
LincAtPenCS <lincoln.mitchell@pencs.com.au>
2024-09-03 17:36:26 +0100

    Update _app.tsx

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -2,9 +2,18 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { MaterialSymbol } from 'react-material-symbols';
 import 'react-material-symbols/rounded'; // Place in your root app file. There are also `sharp` and `outlined` variants.
+import root from 'react-shadow';
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MaterialSymbol icon="folder" size={24} fill grade={-25} color='lime' />
+    <div>    
+      <q style={{color: "red"}}>This Icon is OUTSIDE the Shadow Dom</q>
+      <MaterialSymbol icon="folder" size={24} fill grade={-25} color='blue' />
+      <root.div className="quote">
+        <q style={{color: "yellow"}}>This Icon is INSDOE the Shadow Dom, but only its name appears</q>
+        <MaterialSymbol icon="folder" size={24} fill grade={-25} color='lime' />
+      </root.div>
+    </div>
   )
 }
